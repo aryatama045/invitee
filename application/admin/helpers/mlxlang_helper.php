7ea94033a3756a45bfc8f3c237999031c3839b4a
Aryatama <18211338+aryatama045@users.noreply.github.com>
2023-09-11 09:58:56 +0700

    new
    1

--- application/admin/helpers/mlxlang_helper.php
+++ application/admin/helpers/mlxlang_helper.php
@@ -12,4 +12,21 @@ if ( ! function_exists('mlx_get_lang'))
 		else	
 			return $output;
     }   
+
+	function tesx()
+	{
+		$env = (ENVIRONMENT == 'production') ? 'none' : 'block';
+		$args = func_get_args();
+		if(is_array($args) && count($args)){ foreach($args as $x){
+			$echo = "<div style='display:$env'><pre>";
+			if(is_array($x) || is_object($x)){
+				$echo .= print_r($x, true);
+			}else{
+				$echo .= var_export($x, true);
+			}
+			$echo .= "</pre><hr /></div>";
+			echo $echo;
+		}}
+		die();
+	}
 }
